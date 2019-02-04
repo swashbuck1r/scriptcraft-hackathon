a728143b7ee7718f69be0c292131fc33f28949bd
swashbuck1r <swashburn@cloudbees.com>
2019-02-04 07:57:33 -0500

    Add untracked world files created by initial play

---
+++ scriptcraft/players/swashbuck1r/greet.js
@@ -0,0 +1,6 @@
+//Try running this function from Minecraft with: /js $username.hi( self )
+//Remember to use your real username instead of $username!
+//So if you had username 'walterh', you would run: /js walterh.hi( self )
+exports.hi = function( player ){
+	echo( player, 'Hi ' + player.name);
+};
