648af2785811db2e7bca909cf65d099e4cda540d
ryanwren <rwren@cloudbees.com>
2019-02-04 16:47:15 -0500

    Rename commands file

--- scriptcraft/plugins/hackathon/rw-hackathon.js
+++ scriptcraft/plugins/hackathon/rw-hackathon.js
@@ -9,3 +9,9 @@ command( 'rw-house', function( parameters, player ) {
     //drone.box('5').up(3).left(4).box('1').turn(3).fwd(5).right().box('1').move('start')
     drone.box0(98,7,3,5).right(3).door().up(3).left(3).prism0(53,7,5)
 });
+
+command( 'rw-text', function( parameters, player ) {
+    var drone = new Drone(player);
+    //drone.box('5').up(3).left(4).box('1').turn(3).fwd(5).right().box('1').move('start')
+    drone.blocktype(' CloudBees\nHack-a-thon', blocks.glowstone)
+});
