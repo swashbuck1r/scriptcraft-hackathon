807ea30cb7a0815fd143409e77a932f0eb79a24f
swashbuck1r <swashburn@cloudbees.com>
2019-02-06 10:52:13 -0500

    adding igloo command

--- scriptcraft/plugins/hackathon/spike.js
+++ scriptcraft/plugins/hackathon/spike.js
@@ -21,3 +21,18 @@ command( 'clear', function( parameters, player ) {
     //drone.box(blocks.wood, size, 10, size) //clear house area
     drone.box(0,size,20,size);
 });
+
+//jsp igloo
+command( 'igloo', function( parameters, player ) {
+    var drone = new Drone(player);
+    drone.chkpt('igloo');
+    //make basic structure
+    drone.hemisphere0(blocks.snow, 7, 'north');
+    drone.right(2).down(2);
+    //clear door
+    drone.back();
+    drone.right(4);
+    drone.up(2);
+    drone.box(0, 2, 3, 2);
+    return drone.move('igloo');
+});
