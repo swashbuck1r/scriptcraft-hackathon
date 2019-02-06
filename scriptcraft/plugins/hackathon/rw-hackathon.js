c401e23938569bd8a256b2550c83179d4cf4ee43
ryanwren <rwren@cloudbees.com>
2019-02-06 11:10:06 -0500

    Ryans new castle and dancefloor commands

--- scriptcraft/plugins/hackathon/rw-hackathon.js
+++ scriptcraft/plugins/hackathon/rw-hackathon.js
@@ -6,12 +6,20 @@ splash = require('sounds');
 
 command( 'rw-house', function( parameters, player ) {
     var drone = new Drone(player);
-    //drone.box('5').up(3).left(4).box('1').turn(3).fwd(5).right().box('1').move('start')
     drone.box0(98,7,3,5).right(3).door().up(3).left(3).prism0(53,7,5)
 });
 
 command( 'rw-text', function( parameters, player ) {
     var drone = new Drone(player);
-    //drone.box('5').up(3).left(4).box('1').turn(3).fwd(5).right().box('1').move('start')
     drone.blocktype(' CloudBees\nHack-a-thon', blocks.glowstone)
+});
+
+command( 'rw-castle', function( parameters, player ) {
+    var drone = new Drone(player);
+    drone.castle()
+});
+
+command( 'rw-dancefloor', function( parameters, player ) {
+    var drone = new Drone(player);
+    drone.dancefloor()
 });
