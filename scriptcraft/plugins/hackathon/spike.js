4d99e2e4d646604071a6b9b92eb99d56dd697905
swashbuck1r <swashburn@cloudbees.com>
2019-02-06 16:22:08 -0500

    Add raptorblue command for house building

--- scriptcraft/plugins/hackathon/spike.js
+++ scriptcraft/plugins/hackathon/spike.js
@@ -39,3 +39,14 @@ command( 'igloo', function( parameters, player ) {
     drone.box(0, 2, 3, 2);
     return drone.move('igloo');
 });
+
+
+
+
+command( 'raptorblue', function( parameters, player ) {
+    var drone = new Drone(player);
+    drone.box0(5, 12, 5, 9).right(4).door().up(5).left(4).prism0(5, 12, 9);
+
+    //drone.box('5').up(3).left(4).box('1').turn(3).fwd(5).right().box('1').move('start')
+    //drone.box0(98,7,3,5).right(3).door().up(3).left(3).prism0(53,7,5)
+});
